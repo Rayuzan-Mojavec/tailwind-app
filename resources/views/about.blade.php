09ffca6edcc0c3b229deba8f5caf2901fb2eb6c0
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2022-09-17 19:10:22 +0700

    akhirnya

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,8 +5,8 @@
         <div class="rounded-3xl border-4 border-slate-700 overflow-hidden dark:text-white dark:border-white dark:shadow-2xl">
 
 
-            <div class="h-80 w-60 m-8 bg-green-700 rounded-xl overflow-hidden">
-                <div class="w-full h-full bg-[url('/public/oigh.jpg')] bg-cover bg-center">
+            <div class="h-80 w-60 m-8 bg-green-700 rounded-xl overflow-hidden border-none">
+                <div class="w-full h-full bg-[url('/public/oigh.jpg')] bg-cover bg-center border-none">
                 </div>
             </div>
             <div class="py-7 px-7">
