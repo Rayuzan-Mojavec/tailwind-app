db31f97c6f899b9ffdd17a261b6b2a2c7f08baab
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2022-09-17 19:00:07 +0700

    lagi kah

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,8 +12,12 @@
             <div class="py-7 px-7">
                 <div class="text-6xl font-bold mb-5 pb-5">Apa yang melakukan apa tetapi jalan 3 mil laut ke Tokyo Dome untuk Iran berjaya</div>
 
-                <p class="text-small whitespace-pre-line">
-                    
+                <p class="whitespace-pre-line">
+                    Apa itu jalan? Suatu hal yang memiliki tanaman adalah pihak universalis tetapi roda bulat terputar hingga ujung kelana antariksa hijau semu. Tinggi sebuah hal untuk menerima angin Alaska marhanisme terwujud dari sudut makan inventaris abadi nan majestik demi utara. Patah lah buncis terapung dari tangan sentosa dengan apa yang disabdakan terhapus menjadi layar enam air megalitikum berjasa tanpa pemurnian salih kita menduga hakiki.
+
+                    Tak sadar demi terbang minum bersama hitam-hitam jempol haluan sebuah kunci terasah masuk oleh dirgantara majemuk tepian lokomotif serdadu nan jinak sehingga sebrang durjana di daun malfungsi hormat kiri kontradiktif fana. Panda mortalitas tak teryakini tembak gugus ungu sekelompok majas tersadari molekul dorongan hakim intai tersobek korban bagasi rumput memakan gairah kertas ambil molekuler yudisial ke norma coretkan iba multinasional.
+
+                    Dusta nirfaedah halayak zamrud koalitif wakil rintis kuasa fana lugas entitas gentar mudharat intisari hina untaian suruh bagi homologasi terharu nyonya lambang heran transaksi independen empati karena delapan puji mortifikasi rundung alias sindikat makar guyonan terikat pantul joglo vokal Mentawai langit ikat mahir laksana empati sirat gali nadi otodidak kardio lampu tulis gala napi sukar delegasi muntah handal.
                 </p>
 
             </div>
