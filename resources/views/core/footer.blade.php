bd50eca0abbbb0c00b9f1adf44624c4a18778c75
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2022-09-29 20:02:40 +0700

    added neat insta trick

--- resources/views/core/footer.blade.php
+++ resources/views/core/footer.blade.php
@@ -1,31 +1,34 @@
-<footer class="text-center text-white bg-[#f1f1f1] dark:bg-slate-900">
+<footer class="text-center text-white bg-[#f1f1f1] dark:bg-slate-900 transition">
     <div class="container py-9 mx-auto">
         <div class="flex justify-center">
             <a href="https://www.youtube.com/channel/UCTEnf1exRgmin6AUCP7z2oQ" class="mx-3" target="_blank">
-                <x-bi-youtube class="text-[#FF0000] w-6 h-6"  />
+                <x-bi-youtube class="text-[#FF0000] w-6 h-6  rounded"/>
             </a>
             <a href="https://www.facebook.com/profile.php?id=100010368942719" class="mx-3" target="_blank">
-                <x-bi-facebook class=" text-[#1778f2] w-6 h-6" />
+                <x-bi-facebook class=" text-[#1778f2] w-6 h-6  rounded"/>
             </a>
             <a href="https://www.instagram.com/rayuzan_m/" class="mx-3" target="_blank">
-                <x-bi-instagram class=" text-black dark:text-white w-6 h-6" />
+                <x-bi-instagram class=" text-white transition bg-[url('/public/ins.jpg')] bg-auto bg-no-repeat bg-center border-2 rounded-lg border-transparent w-6 h-6"/>
             </a>
             <a href="https://twitter.com/Rayuzan_M" class="mx-3" target="_blank">
-                <x-bi-twitter class=" text-[#1DA1F2] w-6 h-6" />
+                <x-bi-twitter class=" text-[#1DA1F2] w-6 h-6  rounded"/>
             </a>
             <a href="https://github.com/Rayuzan-Mojavec" class="mx-3" target="_blank">
-                <x-bi-github class=" text-black dark:text-white w-6 h-6" />
+                <x-bi-github class=" text-black dark:text-white transition w-6 h-6  rounded"/>
             </a>
             <a href="https://www.reddit.com/user/Rayuzan_Mojavec" class="mx-3" target="_blank">
-                <x-bi-reddit class=" text-[#FF4500] w-6 h-6" />
+                <x-bi-reddit class=" text-[#FF4500] w-6 h-6  rounded"/>
             </a>
             <a href="https://discord.gg/GNqJ7Q2PCn" class="mx-3" target="_blank">
-                <x-bi-discord class=" text-[#7289DA] w-6 h-6" />
+                <x-bi-discord class=" text-[#7289DA] w-6 h-6  rounded"/>
+            </a>
+            <a href="https://steamcommunity.com/profiles/76561198828589517/" class="mx-3" target="_blank">
+                <x-bi-steam class=" text-black dark:text-white transition w-6 h-6  rounded"/>
             </a>
         </div>
     </div>
 
-    <div class="text-center text-gray-700 dark:text-white p-4 bg-[#f1f1f1] dark:bg-slate-900">
-        © 2021 Copyright: Ningen Sastrawijaya
+    <div class="text-center text-gray-700 dark:text-white p-4 bg-[#f1f1f1] dark:bg-slate-900 transition">
+        &copy; 2021 Copyright: Ningen Sastrawijaya Enterprises&trade;
     </div>
 </footer>
