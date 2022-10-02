903b00f023f466694cd97795fb4b75c4c133f5dc
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2022-10-02 15:34:50 +0700

    changed my mind, use npm instead

--- resources/views/core/footer.blade.php
+++ resources/views/core/footer.blade.php
@@ -1,31 +1,32 @@
 <footer class="text-center text-white bg-[#f1f1f1] dark:bg-slate-900 duration-500 transition">
     <div class="container py-9 mx-auto">
+
         <div class="flex justify-center">
             <a href="https://www.youtube.com/channel/UCTEnf1exRgmin6AUCP7z2oQ" class="mx-3" target="_blank">
-                <x-bi-youtube class="text-[#FF0000] w-6 h-6  rounded"/>
+                <i class="bi bi-youtube text-[#FF0000]   rounded text-2xl"></i>
             </a>
             <a href="https://www.facebook.com/profile.php?id=100010368942719" class="mx-3" target="_blank">
-                <x-bi-facebook class=" text-[#1778f2] w-6 h-6 bg-white rounded-full"/>
+                <i class=" text-[#1778f2] bi bi-facebook  text-2xl"></i>
             </a>
             <a href="https://www.instagram.com/rayuzan_m/" class="mx-3" target="_blank">
-                <x-bi-instagram class=" text-white transition bg-[url('/public/sin.jpg')] bg-[length:24px_24px] bg-no-repeat bg-center border-4 rounded-lg border-transparent w-6 h-6"/>
+                <i class="bi bi-instagram  transition text-black dark:text-white text-2xl"></i>
             </a>
             <a href="https://twitter.com/Rayuzan_M" class="mx-3" target="_blank">
-                <x-bi-twitter class=" text-[#1DA1F2] w-6 h-6  rounded"/>
+                <i  class="bi-twitter bi text-[#1DA1F2]   rounded text-2xl"></i>
             </a>
             <a href="https://github.com/Rayuzan-Mojavec" class="mx-3" target="_blank">
-                <x-bi-github class=" text-black dark:text-white transition duration-500 w-6 h-6  rounded"/>
+                <i  class="bi-github bi text-black dark:text-white transition duration-500   rounded text-2xl"></i>
             </a>
             <a href="https://www.reddit.com/user/Rayuzan_Mojavec" class="mx-3" target="_blank">
-                <x-bi-reddit class=" text-[#FF4500] w-6 h-6 bg-white  rounded-full"/>
+                <i class="bi bi-reddit text-[#FF4500]  text-2xl"></i>
             </a>
             <a href="https://discord.gg/GNqJ7Q2PCn" class="mx-3" target="_blank">
-                <x-bi-discord class=" text-[#7289DA] w-6 h-6  rounded"/>
+                <i class="bi bi-discord text-[#7289DA]   rounded text-2xl"></i>
             </a>
             <a href="https://steamcommunity.com/profiles/76561198828589517/" class="mx-3" target="_blank">
-                <x-bi-steam class=" bg-white  rounded-full text-black  transition w-6 h-6 "/>
+                <i class="bi bi-steam    text-black  transition   text-2xl"></i>
             <a href="https://www.twitch.tv/rayuzanm" class="mx-3" target="_blank">
-                <x-bi-twitch class=" text-[#9146FF] transition w-6 h-6 "/>
+                <i class="bi bi-twitch text-[#9146FF] transition   text-2xl"></i>
             </a>
         </div>
     </div>
--- resources/views/core/header.blade.php
+++ resources/views/core/header.blade.php
@@ -1,12 +1,16 @@
 <nav
 class="flex flex-row text-left justify-center py-4 px-6 dark:bg-slate-900 bg-gray-100 items-baseline w-full">
-<div class="mx-5 ">
-    <a href="/" class="text-xl text-black dark:text-slate-200 font-bold">Home</a>
-</div>
-<div class="mx-5 ">
-    <a href="/about" class="text-xl text-black  dark:text-slate-200 font-bold">About</a>
-</div>
-<div class="mx-5 ">
-    <a href="/tables" class="text-xl text-black  dark:text-slate-200 font-bold">Tables</a>
-</div>
+<ul class="navbar-nav inline-flex">
+    <li class="nav-item mx-4">
+        <a class="nav-link text-xl font-semibold active:text-black {{ Request::is('/') ? 'text-black' : ' text-gray-500 hover:text-gray-700'}} " href="/">Home</a>
+    </li>
+    <li class="nav-item mx-4">
+        <a class="nav-link text-xl font-semibold active:text-black {{ Request::is('about') ? 'text-black' : ' text-gray-500 hover:text-gray-700'}}"
+            href="/about"><i class="bi bi-arrow-down-left-square"></i>About</a>
+    </li>
+    <li class="nav-item mx-4">
+        <a class="nav-link text-xl font-semibold active:text-black {{ Request::is('tables') ? 'text-black' : ' text-gray-500 hover:text-gray-700'}}"
+            href="/tables">Tables</a>
+    </li>
+</ul>
 </nav>
